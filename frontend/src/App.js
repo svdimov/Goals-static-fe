f3aa7f22a0982f6ad9382df7a58eac2e868119f0
stefoto85 <you@example.com>
2026-04-13 18:14:37 +0300

    fix App respomse to azure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('https://goal-app-backend.azurewebsites.net/goals');
 
         const resData = await response.json();
 
